9236029403ea4853b36a35ee03af3bf26c3b96d6
Weston <classicmclive@gmail.com>
2023-05-24 12:01:52 -0400

    Update App.svelte

--- twf3/src/App.svelte
+++ twf3/src/App.svelte
@@ -5,7 +5,7 @@
   }
 </script>
 <style>
-  h1{border-bottom: 1px solid black;font-weight: 10;font-size:30px;}
+  h1{border-bottom: 1px solid black;font-weight: 10;font-size:25px;}
   src{font-weight: 10;font-size:26px;}
   main{margin-left:10%;margin-right:10%;} 
   main img{width:200px!important;height:263px!important;}
@@ -50,32 +50,32 @@
       Weston Snow also known as וועסטון שניי is a student currently attending a secondary school in 
       the United States of America known for language learning, conservative leaning politics, and 
       web development. Born in the early 2,000s in a suburban community Weston Snow was raised a
-      Mormon and still continuing in the religion to the current day. 
+      <a href="https://en.wikipedia.org/wiki/Mormons">Mormon</a> and still continuing in the religion to the current day. 
     </p>
     <!-- Mormonism -->
     <h1 class="fix">Life in the Mormon religion</h1>
     <p>
       Both parents, as Mormons from Utah and Seatle respectively, were raised in the religion and
-      Rasied both Weston and his siblings in the religion of Mormonism and all siblings still 
+      Rasied both Weston and his siblings in the religion of <a href="ttps://en.hwikipedia.org/wiki/Mormonism">Mormonism</a> and all siblings still 
       practice the religion currently. Weston progressed through primary and early sunday school,
-      formerly known as "ctr" classes, the deacon, teacher, and currently in the preists quorum. 
+      classes, the deacon, teacher, and currently in the preists quorum. 
     </p>
     <!-- Web dev -->
     <h1 class="fix">Web development</h1>
     <p>
-      Starting with khan academy in the 7th grade, Weston progressed coding throughout many different
+      Starting with <a href="https://en.wikipedia.org/wiki/Khan_Academy">khan academy</a> in the 7th grade, Weston progressed coding throughout many different
       languages and engines. When begininning with khan academy Weston took only the JS game 
       development course with only a minimal use of other courses on khan academy such as the HTML
       course. With minimal time put off for coding at that point Weston's coding progressed very 
-      little. Eventually learning about the Minecraft raspberry pi from a video posted on Youtube
-      by, Antvenom, Weston ordered a raspberry pi zero. [...] After learning Javascript with normal
+      little. Eventually learning about the Minecraft <a href="https://en.wikipedia.org/wiki/Raspberry_Pi">raspberry pi</a> from a video posted on Youtube
+      by, Antvenom, Weston ordered a raspberry pi zero. <a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">[1]</a> After learning <a href="https://en.wikipedia.org/wiki/JavaScript">Javascript</a> with normal
       HTML Weston created multiple projects as listed below.
       <br/>
       <br/>
-      • Project Darkness [...]<br/>
-      • The last leaf [...]<br/>
-      • Untitled fish game [...]<br/>
-      • Rock Simulator 2022 [...]
+      • Project Darkness <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+      • The last leaf <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+      • Untitled fish game <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+      • Rock Simulator 2022 <a href="https://classicmc-studios.github.io/">[2]</a>
       <br/>
       <br/>
       Other projects not listed in there may include "A final phone call" made in wick editor and
@@ -83,7 +83,7 @@
       (excluding Untitled fish game, and Rock Simulator) are cancelled or unfinished. Eventually
       moving on to HTML as a primary coding language Weston left game development up untill now.
       Originally starting in unmodified HTML Weston tried multiple Javascript frameworks including,
-      React, Astro, and Svelte. Weston is currently coding mostly in Svelte for recent projects. [...] 
+      <a href="https://en.wikipedia.org/wiki/React_(software)">React</a>, Astro, and <a href="https://en.wikipedia.org/wiki/Svelte">Svelte</a>. Weston is currently coding mostly in Svelte for recent projects. [...] 
       <br/>
       <br/>
       One of the most notable projects Weston made in Svelte was Guyot. Guyot was an orginization 
@@ -98,20 +98,20 @@
       <br/>
       <br/>
       Other projects created by Weston include, MossJS a minimalist game engine, PlutoCSS a css 
-      mini design framework for both CSS and ReactNative.
+      mini design framework for both CSS and <a href="https://en.wikipedia.org/wiki/React_Native">ReactNative</a>.
     </p>
     <h1>Language learning and creation</h1>
     Weston started learning French in the 7th grade leading him towards furthering his language goals.
     Weston continued learning French starting from homeschool (only in 7th grade) into French in 
-    secondary and primary school. Weston, after learning about Duolingo, decided on learning the
+    secondary and primary school. Weston, after learning about <a href="https://en.wikipedia.org/wiki/Duolingo">Duolingo</a>, decided on learning the
     language of Norwegian almost completly on Duolingo. Weston continued learning Norwegian until
     late 2022 in which he switched over too Yiddish for around 2 months. Throughout this time
     Weston picked up on basic conversation skills in languages such as German(Deutsch). Currently Weston
     is learning Dutch(Nederlands) in preperation to visit Belgium.
     <br/>
     <br/>
-    Weston begin constructing conlangs in 2021. Weston, along with his brother, constructed Kisou 
-    A conlang with english derived grammar and heavy use of loanwords from a collection of sources
+    Weston begin constructing <a href="https://en.wikipedia.org/wiki/Constructed_language">conlangs</a> in 2021. Weston, along with his brother, constructed Kisou 
+    A <a href="https://en.wikipedia.org/wiki/Constructed_language">conlang</a> with english derived grammar and heavy use of loanwords from a collection of sources
     including, English, French, and Norwegian.
     <br/>
     <br/>
@@ -135,12 +135,129 @@
     <br/>
     After creating Kuosoian Weston spent time learning Yiddish and eventually decided creating
     a final conlang. The conlang was called Garlandic and used loanwords from Cornish, English, 
-    and Varmlandic. This conlang remains unfinished currently.
+    and <a href="https://en.wikipedia.org/wiki/V%C3%A4rmland_County">Varmlandic</a>. This conlang remains unfinished currently.
     <br/>
     <br/>
     <em>&emsp; - Phrases in Garlandic</em><br/>
     Hastum ré a dama iin a man? Ay fursta nisht iin ees furstem nisht! Studéa ú uft Garlandik?
     <br/>
     <h1><src>Sources</src></h1>
+    1.<a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">^</a> Antvenom (2017). <a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">MINECRAFT on a $10 COMPUTER?!</a>. Retrieved 2023-05-24.
+    <br/>
+    2.<a href="https://classicmc-studios.github.io/">^</a> Snow, Weston (2023). <a href="https://classicmc-studios.github.io/">classicmc-studios.github.io</a>. Retrieved 2023-05-24.
+    <br/>
+    <br/>
   </main>
+  {:else}
+  <p align="center">
+    <top><em>TheWestonFiles</em></top><br/>
+    <topSub>The <b>Wikipedia</b> for <em>Nerds</em></topSub>
+  </p>
+  <p align="center"><em>This article may be biased<br/>Viewer Discretion is advised</em></p><br/>
+  <div class="mobile">
+    <h1>Weston Snow</h1>
+    <p align="left"><img class="" style="border-radius:70px;" src="src/band.jpg"/></p>
+        <!-- Introduction -->
+        <p>
+          Weston Snow also known as וועסטון שניי is a student currently attending a secondary school in 
+          the United States of America known for language learning, conservative leaning politics, and 
+          web development. Born in the early 2,000s in a suburban community Weston Snow was raised a
+          <a href="https://en.wikipedia.org/wiki/Mormons">Mormon</a> and still continuing in the religion to the current day. 
+        </p>
+        <!-- Mormonism -->
+        <h1 class="">Life in the Mormon religion</h1>
+        <p>
+          Both parents, as Mormons from Utah and Seatle respectively, were raised in the religion and
+          Rasied both Weston and his siblings in the religion of <a href="ttps://en.hwikipedia.org/wiki/Mormonism">Mormonism</a> and all siblings still 
+          practice the religion currently. Weston progressed through primary and early sunday school,
+          classes, the deacon, teacher, and currently in the preists quorum. 
+        </p>
+        <!-- Web dev -->
+        <h1 class="">Web development</h1>
+        <p>
+          Starting with <a href="https://en.wikipedia.org/wiki/Khan_Academy">khan academy</a> in the 7th grade, Weston progressed coding throughout many different
+          languages and engines. When begininning with khan academy Weston took only the JS game 
+          development course with only a minimal use of other courses on khan academy such as the HTML
+          course. With minimal time put off for coding at that point Weston's coding progressed very 
+          little. Eventually learning about the Minecraft <a href="https://en.wikipedia.org/wiki/Raspberry_Pi">raspberry pi</a> from a video posted on Youtube
+          by, Antvenom, Weston ordered a raspberry pi zero. <a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">[1]</a> After learning <a href="https://en.wikipedia.org/wiki/JavaScript">Javascript</a> with normal
+          HTML Weston created multiple projects as listed below.
+          <br/>
+          <br/>
+          • Project Darkness <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+          • The last leaf <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+          • Untitled fish game <a href="https://classicmc-studios.github.io/">[2]</a><br/>
+          • Rock Simulator 2022 <a href="https://classicmc-studios.github.io/">[2]</a>
+          <br/>
+          <br/>
+          Other projects not listed in there may include "A final phone call" made in wick editor and
+          others. It is worth noting that all listed projects above 
+          (excluding Untitled fish game, and Rock Simulator) are cancelled or unfinished. Eventually
+          moving on to HTML as a primary coding language Weston left game development up untill now.
+          Originally starting in unmodified HTML Weston tried multiple Javascript frameworks including,
+          <a href="https://en.wikipedia.org/wiki/React_(software)">React</a>, Astro, and <a href="https://en.wikipedia.org/wiki/Svelte">Svelte</a>. Weston is currently coding mostly in Svelte for recent projects. [...] 
+          <br/>
+          <br/>
+          One of the most notable projects Weston made in Svelte was Guyot. Guyot was an orginization 
+          created to fix the blocking of emulation in the American school system. Guyot has 2 members 
+          with many other helpers who collaborated on the project. Guyot has created the projects 
+          listed below.
+          <br/>
+          <br/>
+          • GuyotJS <em>emulation</em><br/>
+          • The Weston Files<br/>
+          • Two recreations of Tusky Time
+          <br/>
+          <br/>
+          Other projects created by Weston include, MossJS a minimalist game engine, PlutoCSS a css 
+          mini design framework for both CSS and <a href="https://en.wikipedia.org/wiki/React_Native">ReactNative</a>.
+        </p>
+        <h1>Language learning and creation</h1>
+        Weston started learning French in the 7th grade leading him towards furthering his language goals.
+        Weston continued learning French starting from homeschool (only in 7th grade) into French in 
+        secondary and primary school. Weston, after learning about <a href="https://en.wikipedia.org/wiki/Duolingo">Duolingo</a>, decided on learning the
+        language of Norwegian almost completly on Duolingo. Weston continued learning Norwegian until
+        late 2022 in which he switched over too Yiddish for around 2 months. Throughout this time
+        Weston picked up on basic conversation skills in languages such as German(Deutsch). Currently Weston
+        is learning Dutch(Nederlands) in preperation to visit Belgium.
+        <br/>
+        <br/>
+        Weston begin constructing <a href="https://en.wikipedia.org/wiki/Constructed_language">conlangs</a> in 2021. Weston, along with his brother, constructed Kisou 
+        A <a href="https://en.wikipedia.org/wiki/Constructed_language">conlang</a> with english derived grammar and heavy use of loanwords from a collection of sources
+        including, English, French, and Norwegian.
+        <br/>
+        <br/>
+        <em>&emsp;- Phrase in Kisou<br/></em>
+        Ki! Be tu Weston. Be tu gry. Be wer 5 tos mer fom. Mer kes sautee Valom ond Shema. Be wer 
+        1 es er be kah wer veser.Be feair vou ender flacebowl er be kuh feair activitay.  
+        Mer es sautea Dayshel. Be feair mer es. Mer veser sautea Lilly. Lilly feairs vou ender 
+        roquet de.Mer veser kos tosh mteae. <br/>
+        <em>(Names have been modified and are placeholder names)</em>
+        <br/>
+        <br/>
+        Shortly after quitting on Kisou, because his brother had no more interest in helping,
+        Weston moved on too creating a "polymorphic" language with heavy loanwords from Greenlandic.
+        This language was designed in the way that to write a sentance you would combine suffixes and 
+        prefixes. Ex. Jtarluchaa Jt-arlu-chaa = I am bad.
+        <br/>
+        <br/>
+        <em>&emsp; - A phrase in Kuosoian</em><br/>
+        Til-yeccJtquatarpervusilaqjtnaqoschk
+        <br/>
+        <br/>
+        After creating Kuosoian Weston spent time learning Yiddish and eventually decided creating
+        a final conlang. The conlang was called Garlandic and used loanwords from Cornish, English, 
+        and <a href="https://en.wikipedia.org/wiki/V%C3%A4rmland_County">Varmlandic</a>. This conlang remains unfinished currently.
+        <br/>
+        <br/>
+        <em>&emsp; - Phrases in Garlandic</em><br/>
+        Hastum ré a dama iin a man? Ay fursta nisht iin ees furstem nisht! Studéa ú uft Garlandik?
+        <br/>
+        <h1><src>Sources</src></h1>
+        1.<a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">^</a> Antvenom (2017). <a href="https://www.youtube.com/watch?v=9JmZkP7XZm4">MINECRAFT on a $10 COMPUTER?!</a>. Retrieved 2023-05-24.
+        <br/>
+        2.<a href="https://classicmc-studios.github.io/">^</a> Snow, Weston (2023). <a href="https://classicmc-studios.github.io/">classicmc-studios.github.io</a>. Retrieved 2023-05-24.
+        <br/>
+        <br/>
+  </div>
 {/if}
