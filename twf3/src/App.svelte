caacfe12359be18b6669ea2134ba64e3c078c08d
ClassicMC <classicmclive@gmail.com>
2023-05-24 03:51:49 +0000

    twf3

---
+++ twf3/src/App.svelte
@@ -0,0 +1,117 @@
+<script>
+  let mobile = false;
+  if(window.innerWidth < window.innerHeight){
+    mobile = true;
+  }
+</script>
+<style>
+  h1{border-bottom: 1px solid black;font-weight: 10;font-size:30px;}
+  src{font-weight: 10;font-size:26px;}
+  main{margin-left:10%;margin-right:10%;} 
+  main img{width:200px!important;height:263px!important;}
+  div img{width: 100px;height:calc(263px/2);}
+  main img{float: right;}
+  div img{float: left;}
+  .fix{margin-right:230px; }
+  p{margin-right: 15px;}
+  a{color:rgb(0, 123, 255);text-decoration: none;}
+  a:hover{text-decoration: underline;}
+  r{color:rgb(255, 0, 0);}
+  cc{color:rgb(0, 183, 255)}
+  top{font-weight: 10;font-size:30px;margin:5%;}
+  topSub{margin:5%;}
+  articleExtra{
+    float: right;
+    margin-right: 5%;
+    padding-top:10px;
+  }
+  .mobile{
+    margin-right: 5%;
+    margin-left: 5%;
+  }
+  .grid{border-right:1px solid gray;background-color:rgb(238,238,238);padding-left:2px;padding-right:2px;}
+  .gridEnd{background-color:rgb(238,238,238);padding-left:2px;padding-right:2px;}
+  table{background-color:rgb(238,238,238);}
+</style>
+{#if !mobile}
+  <articleExtra><p align="center">This article may be biased <br/> Discretion is advised</p></articleExtra>
+  <top>
+    <em>The Weston Files</em>
+  </top><br/>
+  <topSub>
+    The <b>Wikipedia</b> for <em>Nerds</em>
+  </topSub>
+  <br/><br/>
+  <main>
+    <h1>Weston Snow</h1>
+    <p align="right"><img src="src/band.jpg" class="imgr bc-border"/></p>
+    <!-- Introduction -->
+    <p>
+      Weston Snow also known as וועסטון שניי is a student currently attending a secondary school in 
+      the United States of America known for language learning, conservative leaning politics, and 
+      web development. Born in the early 2,000s in a suburban community Weston Snow was raised a
+      Mormon and still continuing in the religion to the current day. 
+    </p>
+    <!-- Mormonism -->
+    <h1 class="fix">Life in the Mormon religion</h1>
+    <p>
+      Both parents, as Mormons from Utah and Seatle respectively, were raised in the religion and
+      Rasied both Weston and his siblings in the religion of Mormonism and all siblings still 
+      practice the religion currently. Weston progressed through primary and early sunday school,
+      formerly known as "ctr" classes, the deacon, teacher, and currently in the preists quorum. 
+    </p>
+    <!-- Web dev -->
+    <h1 class="fix">Web development</h1>
+    <p>
+      Starting with khan academy in the 7th grade, Weston progressed coding throughout many different
+      languages and engines. When begininning with khan academy Weston took only the JS game 
+      development course with only a minimal use of other courses on khan academy such as the HTML
+      course. With minimal time put off for coding at that point Weston's coding progressed very 
+      little. Eventually learning about the Minecraft raspberry pi from a video posted on Youtube
+      by, Antvenom, Weston ordered a raspberry pi zero. [...] After learning Javascript with normal
+      HTML Weston created multiple projects as listed below.
+      <br/>
+      <br/>
+      • Project Darkness [...]<br/>
+      • The last leaf [...]<br/>
+      • Untitled fish game [...]<br/>
+      • Rock Simulator 2022 [...]
+      <br/>
+      <br/>
+      Other projects not listed in there may include "A final phone call" made in wick editor and
+      others. It is worth noting that all listed projects above 
+      (excluding Untitled fish game, and Rock Simulator) are cancelled or unfinished. Eventually
+      moving on to HTML as a primary coding language Weston left game development up untill now.
+      Originally starting in unmodified HTML Weston tried multiple Javascript frameworks including,
+      React, Astro, and Svelte. Weston is currently coding mostly in Svelte for recent projects. [...] 
+      <br/>
+      <br/>
+      One of the most notable projects Weston made in Svelte was Guyot. Guyot was an orginization 
+      created to fix the blocking of emulation in the American school system. Guyot has 2 members 
+      with many other helpers who collaborated on the project. Guyot has created the projects 
+      listed below.
+      <br/>
+      <br/>
+      • GuyotJS <em>emulation</em><br/>
+      • The Weston Files<br/>
+      • Two recreations of Tusky Time
+      <br/>
+      <br/>
+      Other projects created by Weston include, MossJS a minimalist game engine, PlutoCSS a css 
+      mini design framework for both CSS and ReactNative.
+    </p>
+    <h1>Language learning and creation</h1>
+    Weston started learning French in the 7th grade leading him towards furthering his language goals.
+    Weston continued learning French starting from homeschool (only in 7th grade) into French in 
+    secondary and primary school. Weston, after learning about Duolingo, decided on learning the
+    language of Norwegian almost completly on Duolingo. Weston continued learning Norwegian until
+    late 2022 in which he switched over too Yiddish for around 2 months. Throughout this time
+    Weston picked up on basic conversation skills in languages such as German(Deutsch). Currently Weston
+    is learning Dutch(Nederlands) in preperation to visit Belgium.
+    <br/>
+    <br/>
+    Weston begin constructing conlangs in 2021.
+    <br/>
+    <h1><src>Sources</src></h1>
+  </main>
+{/if}
