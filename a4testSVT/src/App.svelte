29e1cb5ea994e80bdbc767a95a27eb6a3d06d825
ClassicMC <classicmclive@gmail.com>
2023-05-31 03:32:59 +0000

    jassss

--- a4testSVT/src/App.svelte
+++ a4testSVT/src/App.svelte
@@ -57,8 +57,38 @@
       <p>&nbsp;<a>Localhost</a><span>&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Coding</span></p>
     </box>
     <p>
-      Multiple websites have been banned prodominantly due to use by Weston. These websites are not closely related however and the reason for the banning of these webistes is still unclear. Wesbites that have been banned include, MyEmulator, Gitpod, and Localhost. Localhost was the most recent ban happening at 12:00 am EST on May 25, 2023. [1]
+      Multiple websites have been banned prodominantly due to use by Weston. These websites are not closely related however and the reason for the banning of these webistes is still unclear. Wesbites that have been banned include, MyEmulator, Gitpod, and <a href="https://en.wikipedia.org/wiki/Localhost">Localhost</a>. Localhost was the most recent ban happening at 12:00 am EST on May 25, 2023. <a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">[1]</a>
     </p>
+    <h1 class="fix">
+      MyEmulator
+    </h1>
+    <p>
+      MyEmulator, being banned on November 6, 2022, was a emulation website contaning <a href="https://en.wikipedia.org/wiki/Atari">atari</a>, <a href="https://en.wikipedia.org/wiki/Nintendo">nintendo</a>, and <a href="https://en.wikipedia.org/wiki/PlayStation_(console)">ps1</a> games. The current ban message is "mature.games". <a href="https://myemulator.online">[2]</a> MyEmulator known to be used regularly by, 3 members of the secondary school. MyEmulator was banned on the morning of November 6, 2022 after being heavily used by a group of people during a marching band overnight. MyEmulator was the first major ban affecting or caused by Weston.
+    </p>
+    <h1>
+      Gitpod
+    </h1>
+    <p>
+      Banned shortly after MyEmulator, Gitpod was banned on the 10th of November, 2022. Gitpod is a online coding website which uses the base <a href="https://en.wikipedia.org/wiki/Visual_Studio_Code">vscode</a> system but including access to a basic terminal, localhost capabilitys, and full extension support. Gitpod was banned in the early morning and first noticed by Weston in the start of the first block. <a href="#">[3]</a> At the time of the ban Gitpod is believed to be only known by Weston as no other person in the secondary school knew about or used it. Weston got in contact with the school support system early in the morning of November 10th at 10:42 AM EST. The filtered email is shown below only removing place and people's names.
+      <br/><br/>
+      Hello [.......] support center,<br/>
+      I am today inquiring on the reason for blocking the website gitpod.io. This website is a great tool to learn how to code and is safe in terms of viruses and inappropriate imagery. It would benefit me and many other students if the website gitpod.io would be available for coding. Some uses of gitpod.io include, a GCC compiler for running C and C++ code, access to a base level terminal with no root computer access, a basic localhost ability for loading react and svelte projects, committing projects to github using the Git commit function, and running python projects in a virtual terminal. To conclude, the continued access to the coding website gitpod.io would be beneficial to myself and other students.
+      <br/>Sincerely, [.......] of [........] Secondary School
+      <br/><br/>
+      After discussing with the schools support center and eventually with the state Weston was able to get Gitpod unblocked. Until the writing of this article, gitpod is still live and accessable from all school <a href="https://en.wikipedia.org/wiki/Chromebook">chromebooks</a>.
+    </p>
+    <h1>
+      Localhost
+    </h1>
+    <p>
+      Localhost was banned at 12:00 AM EST on May 25, 2023. Localhost, only accessable from gitpod and github codespaces, was most likely only used by Weston although there is a possibility that 1 more student in the secondary school knows and uses it regularly. The reason for Localhost's ban is currently unknown and its ban message is "unknown" as stated from the ban website. A posibility for the ban may be that it could be possible to say or show inappropriate images or text through a localhost although this would be very unlikely due to the fact that localhost is only used by people who know how to code and <a href="https://en.wikipedia.org/wiki/HTML_element">iframe</a> can not interact with banned websites. Happening on May 30th an exploit to the localhost block has been found. The way to bypass the restriction is to click the "make public" button when calling "npm run dev".
+    </p>
+    <h1><src>Sources</src></h1>
+    1.<a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">^</a> Snow, Weston (2023). <a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/</a>  Gitpod<br/>
+    2.<a href="https://myemulator.online">^</a> (2022). <a href="https://myemulator.online">myemulator.online</a>  MyEmulator<br/>
+    3.<a href="#">^</a> Snow, Weston (2022). gmail sent at 10:42AM  Gmail
+    <br/>
+    <br/>
   </main>
   {:else}
   <p align="center">
@@ -75,7 +105,37 @@
       <p><a>LocalHost:</a><span>Coding</span></p>
     </mobileBox>
     <p>
-      Multiple websites have been banned prodominantly due to use by Weston. These websites are not closely related however and the reason for the banning of these webistes is still unclear. Wesbites that have been banned include, MyEmulator, Gitpod, and Localhost. Localhost was the most recent ban happening at 12:00 am EST on May 25, 2023. [1]
+      Multiple websites have been banned prodominantly due to use by Weston. These websites are not closely related however and the reason for the banning of these webistes is still unclear. Wesbites that have been banned include, MyEmulator, Gitpod, and <a href="https://en.wikipedia.org/wiki/Localhost">Localhost</a>. Localhost was the most recent ban happening at 12:00 am EST on May 25, 2023. <a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">[1]</a>
     </p>
+    <h1 class="">
+      MyEmulator
+    </h1>
+    <p>
+      MyEmulator, being banned on November 6, 2022, was a emulation website contaning <a href="https://en.wikipedia.org/wiki/Atari">atari</a>, <a href="https://en.wikipedia.org/wiki/Nintendo">nintendo</a>, and <a href="https://en.wikipedia.org/wiki/PlayStation_(console)">ps1</a> games. The current ban message is "mature.games". <a href="https://myemulator.online">[2]</a> MyEmulator known to be used regularly by, 3 members of the secondary school. MyEmulator was banned on the morning of November 6, 2022 after being heavily used by a group of people during a marching band overnight. MyEmulator was the first major ban affecting or caused by Weston.
+    </p>
+    <h1>
+      Gitpod
+    </h1>
+    <p>
+      Banned shortly after MyEmulator, Gitpod was banned on the 10th of November, 2022. Gitpod is a online coding website which uses the base <a href="https://en.wikipedia.org/wiki/Visual_Studio_Code">vscode</a> system but including access to a basic terminal, localhost capabilitys, and full extension support. Gitpod was banned in the early morning and first noticed by Weston in the start of the first block. <a href="#">[3]</a> At the time of the ban Gitpod is believed to be only known by Weston as no other person in the secondary school knew about or used it. Weston got in contact with the school support system early in the morning of November 10th at 10:42 AM EST. The filtered email is shown below only removing place and people's names.
+      <br/><br/>
+      Hello [.......] support center,<br/>
+      I am today inquiring on the reason for blocking the website gitpod.io. This website is a great tool to learn how to code and is safe in terms of viruses and inappropriate imagery. It would benefit me and many other students if the website gitpod.io would be available for coding. Some uses of gitpod.io include, a GCC compiler for running C and C++ code, access to a base level terminal with no root computer access, a basic localhost ability for loading react and svelte projects, committing projects to github using the Git commit function, and running python projects in a virtual terminal. To conclude, the continued access to the coding website gitpod.io would be beneficial to myself and other students.
+      <br/>Sincerely, [.......] of [........] Secondary School
+      <br/><br/>
+      After discussing with the schools support center and eventually with the state Weston was able to get Gitpod unblocked. Until the writing of this article, gitpod is still live and accessable from all school <a href="https://en.wikipedia.org/wiki/Chromebook">chromebooks</a>.
+    </p>
+    <h1>
+      Localhost
+    </h1>
+    <p>
+      Localhost was banned at 12:00 AM EST on May 25, 2023. Localhost, only accessable from gitpod and github codespaces, was most likely only used by Weston although there is a possibility that 1 more student in the secondary school knows and uses it regularly. The reason for Localhost's ban is currently unknown and its ban message is "unknown" as stated from the ban website. A posibility for the ban may be that it could be possible to say or show inappropriate images or text through a localhost although this would be very unlikely due to the fact that localhost is only used by people who know how to code and <a href="https://en.wikipedia.org/wiki/HTML_element">iframe</a> can not interact with banned websites. Happening on May 30th an exploit to the localhost block has been found. The way to bypass the restriction is to click the "make public" button when calling "npm run dev".
+    </p>
+    <h1><src>Sources</src></h1>
+    1.<a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">^</a> Snow, Weston (2023). <a href="https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/">https://5173-classicmcst-thewordofwe-z75coxv8eqs.ws-us98.gitpod.io/</a>  Gitpod<br/>
+    2.<a href="https://myemulator.online">^</a> (2022). <a href="https://myemulator.online">myemulator.online</a>  MyEmulator<br/>
+    3.<a href="#">^</a> Snow, Weston (2022). gmail sent at 10:42AM  Gmail
+    <br/>
+    <br/>
   </div>
 {/if}
